fbc48f6ed025fdae1f44d86c9be716f2fdd53570
c0nrad <sclarsen@mtu.edu>
2013-03-17 17:10:06 -0400

    First commit

---
+++ c0nradsc0rner/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import patterns, include, url
+from blog.models import Blog
+
+from blog import views
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    (r'^$', 'blog.views.index'),
+
+    url(
+        r'^blog/view/(?P<slug>[^\.]+).html', 
+        views.view_post, 
+        name='view_blog_post'),
+    url(
+        r'^blog/category/(?P<slug>[^\.]+).html', 
+        views.view_category, 
+        name='view_blog_category'),
+                       
+                       # Examples:
+    # url(r'^$', 'c0nradsc0rner.views.home', name='home'),
+    # url(r'^c0nradsc0rner/', include('c0nradsc0rner.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
